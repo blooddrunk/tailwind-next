05383c581ae3cc4fd499e0bb36f355da2a78dea9
Ryder Xie <jelinenaro@gmail.com>
2019-07-21 20:07:42 +0800

    initial commit

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const withPlugins = require('next-compose-plugins');
+const withCSS = require('@zeit/next-css');
+const withPurgeCss = require('next-purgecss');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
+
+module.exports = withPlugins([withCSS,
+ [withPurgeCss, {
+  purgeCssEnabled: ({ dev, isServer }) => (!dev && !isServer) // Only enable PurgeCSS for client-side production builds
+}
+], {
+  webpack(config, options) {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.join(__dirname, './tsconfig.json'),
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      }),
+    ];
+
+    return config;
+  },
+});
