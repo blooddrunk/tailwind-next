1dfd1939ee9f4e80b34da6d834997eed8f55769c
Ryder Xie <jelinenaro@gmail.com>
2019-07-29 18:42:52 +0800

    correct tsconfig

--- next.config.js
+++ next.config.js
@@ -4,19 +4,24 @@ const withPurgeCss = require('next-purgecss');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const path = require('path');
 
-module.exports = withPlugins([withCSS,
- [withPurgeCss, {
-  purgeCssEnabled: ({ dev, isServer }) => (!dev && !isServer) // Only enable PurgeCSS for client-side production builds
-}
-], {
-  webpack(config, options) {
-    config.resolve.plugins = [
-      new TsconfigPathsPlugin({
-        configFile: path.join(__dirname, './tsconfig.json'),
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-      }),
-    ];
+module.exports = withPlugins([
+  withCSS,
+  [
+    withPurgeCss,
+    {
+      purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer, // Only enable PurgeCSS for client-side production builds
+    },
+  ],
+  {
+    webpack(config, options) {
+      config.resolve.plugins = [
+        new TsconfigPathsPlugin({
+          configFile: path.join(__dirname, './tsconfig.json'),
+          extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        }),
+      ];
 
-    return config;
+      return config;
+    },
   },
-});
+]);
